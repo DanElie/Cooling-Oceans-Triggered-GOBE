52d1cdf44f358f96ca53223eafd69ff0432b7fe8
Daniel Eliahou <114865367+DanElie@users.noreply.github.com>
2022-10-25 12:21:39 +0200

    Update Step9_Gradient.m

--- Step9_Gradient.m
+++ Step9_Gradient.m
@@ -23,6 +23,25 @@ xlabel('Geological time');
 ylabel('latitude');
 %title('Song Ordovician LDG');
 
-%% save
+%% Gradient as median of MATDEF
 
-saveas(gcf,'Gradient_Song.pdf')
+for i = 1:7
+    clear GL
+    GL = Song(i).MATDEF;
+    GL = nanmedian(GL, 2);
+    LBG(:,i) = GL;
+end
+
+figure;
+hold on;
+contourf([Song.geotimes], latdef, LBG);
+caxis([0 7000]);
+colormap('jet');
+h = colorbar;
+h.Label.String = 'Pseudospecies richness';
+xlabel('Geological time');
+ylabel('latitude');
+title('LBG (mediane)')
+
+%% save
+exportgraphics(gcf,'Gradient_Song.tif','Resolution',300)
